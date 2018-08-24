2d800733dc3e6dcb14f545d761014690d743f3dc
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-08-23 22:18:04 -0700

    Now includes accounts backend api

--- waker.py
+++ waker.py
@@ -1,45 +1,24 @@
 import urllib.request
 from datetime import datetime
 
+sleeping_beauties = ['https://antalmanac.herokuapp.com/',
+                     'http://summer18.herokuapp.com/',
+                     'https://websocserver.herokuapp.com/',
+                     'https://reactant.herokuapp.com/',
+                     'https://aareact.herokuapp.com/',
+                     'https://antal.herokuapp.com/',
+                     'https://gentle-inlet-23513.herokuapp.com/']
+                     
 now_time = datetime.now().time()
 print(now_time)
 if True:#(now_time > datetime.strptime('15:00', '%H:%M').time() and now_time < datetime.strptime('23:59', '%H:%M').time()) or now_time < datetime.strptime('5:00', '%H:%M').time():
-    try:
-        data = urllib.request.urlopen('https://antalmanac.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
-    try:
-        data = urllib.request.urlopen('http://summer18.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
-    try:
-        data = urllib.request.urlopen('https://websocserver.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
-    try:
-        data = urllib.request.urlopen('https://reactant.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
-    try:
-        data = urllib.request.urlopen('https://aareact.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
-    try:
-        data = urllib.request.urlopen('https://antal.herokuapp.com/')
-        data.read()
-        data.close()
-    except:
-        pass
+    for beauty in sleeping_beauties:
+        try:
+            data = urllib.request.urlopen(beauty)
+            data.read()
+            data.close()
+        except:
+            pass
     
     print('woke')
 else:
