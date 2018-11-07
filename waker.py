e66db55ec5d2e7964a3295c2c83b0058ae31cd4d
Rango <34990962+the-rango@users.noreply.github.com>
2018-11-06 21:34:09 -0800

    Included info page as well

--- waker.py
+++ waker.py
@@ -1,14 +1,14 @@
 import urllib.request
 from datetime import datetime
 
-sleeping_beauties = ['https://antalmanac.herokuapp.com/',
+sleeping_beauties = ['https://the-antalmanac.herokuapp.com/',
                      'http://summer18.herokuapp.com/',
                      'https://websocserver.herokuapp.com/',
-                     'https://reactant.herokuapp.com/',
-                     'https://aareact.herokuapp.com/',
-                     'https://antal.herokuapp.com/',
                      'https://pleaserunforme.herokuapp.com/',
                      'https://gentle-inlet-23513.herokuapp.com/']
+                     #'https://reactant.herokuapp.com/',
+                     #'https://aareact.herokuapp.com/',
+                     #'https://antal.herokuapp.com/']
                      
 now_time = datetime.now().time()
 print(now_time)
