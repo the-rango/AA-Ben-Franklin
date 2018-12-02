c7df4a2be0d6b176e8453ad09ec79f2cb28e472f
Rango <34990962+the-rango@users.noreply.github.com>
2018-12-02 11:18:34 -0800

    Wakes Paul Revere

--- waker.py
+++ waker.py
@@ -1,7 +1,6 @@
 import urllib.request
 
-sleeping_beauties = ['https://the-antalmanac.herokuapp.com/',
-                     'https://gentle-inlet-23513.herokuapp.com/']
+sleeping_beauties = ['https://mediaont.herokuapp.com/']
 
 for beauty in sleeping_beauties:
     try:
