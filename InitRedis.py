8fbe48c57129ade30be96bba82a4cb24bbf86892
Rango <34990962+the-rango@users.noreply.github.com>
2019-02-26 20:56:10 -0800

    update qtr

--- InitRedis.py
+++ InitRedis.py
@@ -4,11 +4,10 @@ import SOCSpider
 import redis
 
 r = redis.from_url(os.environ.get('REDISCLOUD_URL'))
-r.flushall()
 
 print('*****Doggo\'s doing its thing*****')
 master_dict = SOCSpider.getAllInfo(SOCSpider.getURL(SOCSpider.getDepts()))
 for code, data in master_dict.items():
     print(code,end=' ')
-    r.set('w'+code, ([data[0]], [data[1]], [data[2]], [data[3]], [data[4]]))
+    r.set('s'+code, ([data[0]], [data[1]], [data[2]], [data[3]], [data[4]]))
 print('done')
